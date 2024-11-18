4ec842524df5fe7a4357ae14f96c7fe9735094ba
gedithejedi <33606955+gedithejedi@users.noreply.github.com>
2024-11-18 14:57:30 +0700

    feat: full flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import { useMutation } from '@tanstack/react-query';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { erc20Abi, isAddress, parseUnits } from 'viem';
-import { getPublicClient, getWalletClient } from "wagmi/actions";
+import { getPublicClient, getWalletClient, waitForTransactionReceipt } from "wagmi/actions";
 import { wagmiAdapter } from '../../utils/config';
 import senderAbi from '../../utils/sender.json';
+import { useAccount } from 'wagmi';
+
+const contractAddress = "0x82D8824255aC1030E5F01Ad7984B505Ad9De0C2D"
 
 function parseAddresses(input: string): Array<{ address: string; amount: string }> {
   const lines = input.trim().split('\n');
@@ -30,12 +33,17 @@ function parseAddresses(input: string): Array<{ address: string; amount: string 
 export default function Home() {
   const [addresses, setAddresses] = useState("");
   const [tokenAddress, setTokenAddress] = useState("");
+  const [hasHydrated, setHasHydrated] = useState(false);
+  const [transactionHash, setTransactionHash] = useState("");
+
+  const { address } = useAccount();
 
   const { mutate: onSend, isPending: isSending } = useMutation({
     mutationFn: async () => {
       const toastId = toast.loading("Sign the transactions to send the tokens.");
 
       try {
+        if (!address) return toast.error("Please connect your wallet.", { id: toastId });
         if (!tokenAddress || !isAddress(tokenAddress))
           return toast.error("Please enter a valid token address.", { id: toastId });
 
@@ -45,17 +53,21 @@ export default function Home() {
 
         const client = getPublicClient(wagmiAdapter.wagmiConfig);
         const walletClient = await getWalletClient(wagmiAdapter.wagmiConfig);
-        const [account] = await walletClient.getAddresses();
 
         if (!client) return console.error("no client");
+
+        const allowance = await client.readContract({
+          address: tokenAddress,
+          abi: erc20Abi,
+          functionName: 'allowance',
+          args: [address, contractAddress],
+        })
 
         const decimals = await client.readContract({
           address: tokenAddress,
           abi: erc20Abi,
           functionName: 'decimals',
         })
-
-        console.log(decimals);
 
         try {
           const addys = parseAddresses(addresses);
@@ -78,23 +90,36 @@ export default function Home() {
           throw new Error("Please make sure the addresses and amounts are in a correct format.");
         }
 
-        console.log({
-          account,
+        try {
+          const totalAmount = parsedAmounts.reduce((acc, curr) => acc + curr, BigInt(0));
+
+          if (allowance < totalAmount) {
+            const { request } = await client.simulateContract({
+              account: address,
+              abi: erc20Abi,
+              address: tokenAddress,
+              functionName: "approve",
+              args: [contractAddress, totalAmount - allowance]
+            });
+            const approveHash = await walletClient.writeContract(request);
+            await waitForTransactionReceipt(wagmiAdapter.wagmiConfig, { hash: approveHash });
+          }
+        } catch (e) {
+          toast.error("Something went wrong while approving the tokens.", { id: toastId });
+          throw new Error("Something went wrong while approving the tokens.");
+        }
+
+        const { request } = await client.simulateContract({
+          account: address,
           abi: senderAbi,
-          address: "contractAddress" as `0x${string}`,
+          address: contractAddress,
           functionName: "disperseToken",
           args: [tokenAddress, parsedAddys, parsedAmounts]
         });
 
-        // const { request } = await client.simulateContract({
-        //   account,
-        //   abi: senderAbi,
-        //   address: contractAddress as `0x${string}`,
-        //   functionName: "disperseToken",
-        //   args: [tokenAddress, parsedAddys, parsedAmounts]
-        // });
-
-        // await walletClient.writeContract(request);
+        const hash = await walletClient.writeContract(request);
+        await waitForTransactionReceipt(wagmiAdapter.wagmiConfig, { hash });
+        setTransactionHash(hash)
         toast.success("Tokens sent successfully.", { id: toastId });
       } catch (error) {
         toast.error("Something went wrong while sending the tokens.", { id: toastId });
@@ -102,6 +127,10 @@ export default function Home() {
       }
     },
   });
+
+  useEffect(() => {
+    setHasHydrated(true);
+  }, [])
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen font-[family-name:var(--font-geist-sans)]">
@@ -116,26 +145,32 @@ export default function Home() {
         </div>
 
         <div className="card w-[700px] shadow-xl p-7 bg-[#efefef]">
-          <div className='flex flex-col gap-4'>
+          {!!transactionHash ? (
+            <div>
+              <h2 className="text-4xl font-bold text-center mb-2">All done! 🚀🚀</h2>
+              <p className="text-left mb-2">Your tokens have been successfully sent. You can view your transaction here:</p>
+              <a className='link' target='_blank' href={`https://hekla.taikoscan.io/tx/${transactionHash}`}>{transactionHash}</a>
+            </div>
+          ) : <div className='flex flex-col gap-4'>
             <label className="form-control w-full">
               <div className="label">
                 <span className="label-text">Token address</span>
               </div>
-              <input value={tokenAddress} onChange={(e) => setTokenAddress(e.target.value)} type="text" placeholder="0xAbC" className="input input-bordered w-full" />
+              <input disabled={isSending} value={tokenAddress} onChange={(e) => setTokenAddress(e.target.value)} type="text" placeholder="0xAbC" className="input input-bordered w-full" />
             </label>
 
             <label className="form-control">
               <div className="label">
                 <span className="label-text">Stakeholder details</span>
               </div>
-              <textarea value={addresses} onChange={(e) => setAddresses(e.target.value)} className="textarea textarea-bordered h-24" placeholder={`0xeD38571DEE9605EDB90323964E2F12Ad026c6C11,10`}></textarea>
+              <textarea disabled={isSending} value={addresses} onChange={(e) => setAddresses(e.target.value)} className="textarea textarea-bordered h-24" placeholder={`0xeD38571DEE9605EDB90323964E2F12Ad026c6C11,10`}></textarea>
             </label>
 
-            <button disabled={isSending} onClick={() => onSend()} className={`btn btn-primary mt-2 font-bold text-white hover:bg-primary-hover hover:text-primary rounded-full text-[16px]`}>
+            <button disabled={isSending || !address || !hasHydrated} onClick={() => onSend()} className={`btn btn-primary mt-2 font-bold text-white hover:bg-primary-hover hover:text-primary rounded-full text-[16px]`}>
               {isSending ? <span className="loading loading-spinner"></span> : ""}
-              Deploy
+              {!!hasHydrated && address ? "Send" : "Connect wallet"}
             </button>
-          </div>
+          </div>}
         </div>
 
       </main >
